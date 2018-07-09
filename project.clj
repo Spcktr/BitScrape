78d96c9359b1927a9b6b823cc76b9952f0d34bd4
Spcktr <Spcktr@users.noreply.github.com>
2018-07-10 08:32:37 +1000

    Added Base Deps

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [ring/ring-core "1.7.0-RC1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-jetty-adpater "1.7.0-RC1"]
+                 [compojure "1.6.1"]
+                 [enlive "1.1.6"]]
   :main ^:skip-aot scrape.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
