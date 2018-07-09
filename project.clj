d1f84f73896dbc86828c20ae532bb80cc08ea56b
Spcktr <Spcktr@users.noreply.github.com>
2018-07-10 08:57:25 +1000

    Fixed typo in deps

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [ring/ring-core "1.7.0-RC1"]
                  [ring/ring-defaults "0.3.2"]
-                 [ring/ring-jetty-adpater "1.7.0-RC1"]
+                 [ring/ring-jetty-adapter "1.7.0-RC1"]
                  [compojure "1.6.1"]
                  [enlive "1.1.6"]]
   :main ^:skip-aot scrape.core
