d8d73c1705b85454871022cefe968f0e304ef01f
Spcktr <Spcktr@users.noreply.github.com>
2018-07-10 08:57:54 +1000

    First test for scrape

--- src/scrape/core.clj
+++ src/scrape/core.clj
@@ -1,7 +1,22 @@
 (ns scrape.core
-  (:gen-class))
+  (:require [net.cgrand.enlive-html :as html]
+        [compojure.core :as compojure]
+        [ring.adpater.jetty :as jetty]
+        [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+
+(defn get-dom
+  []
+  (html/html-snippet
+    (:body @(compojure/GET "http://nanopool.org"))))
+
+(defn extract-coin-name
+  [dom]
+  (map
+    (comp first :coin-name)
+      (html/select dom [:a.panel-title])))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (let [coin-name (extract-coin-name (get-dome))]
+    print coin-name))
