0cac52a73612fea5c18612b49f25cb35921d025e
Spcktr <Spcktr@users.noreply.github.com>
2018-07-10 10:19:11 +1000

    Refresh to raw file

--- src/scrape/core.clj
+++ src/scrape/core.clj
@@ -1,22 +1,6 @@
 (ns scrape.core
-  (:require [net.cgrand.enlive-html :as html]
-        [compojure.core :as compojure]
-        [ring.adpater.jetty :as jetty]
-        [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+  (:gen-class))
 
-
-(defn get-dom
-  []
-  (html/html-snippet
-    (:body @(compojure/GET "http://nanopool.org"))))
-
-(defn extract-coin-name
-  [dom]
-  (map
-    (comp first :coin-name)
-      (html/select dom [:a.panel-title])))
-
-(defn -main
-  []
-  (let [coin-name (extract-coin-name (get-dome))]
-    print coin-name))
+  (defn -main
+    [& args]
+    (println "Hello, World!"))
